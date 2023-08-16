58db07c99d9a42d72a0a2edc3f482d85c6a4be99
Kaito-Dogi <kendobu0405@gmail.com>
2023-08-16 15:37:24 +0900

    ➕ sort-keys-custom-order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import { FC } from 'react'
+
 export default function Home() {
   return (
-    <main className="grid h-full place-items-center">
-      <h1 className="text-center">jiswitch prototype</h1>
+    <main className='grid h-full place-items-center'>
+      <h1 className='text-center'>jiswitch prototype</h1>
     </main>
-  );
+  )
 }
+
+type Props = {
+  a: string
+  bb: string
+  ccc: string
+}
+
+export const Hoge: FC<Props> = ({ a, bb, ccc }) => <p>{`${a + bb + ccc}`}</p>
