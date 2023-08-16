2e35be826fbf0dd3abe4c9f2690893b9909c13e5
Kaito-Dogi <kendobu0405@gmail.com>
2023-08-17 06:36:49 +0900

    ✨ プレビュー画像を表示する処理を実装

--- src/app/chat/components/ChatForm/ChatForm.tsx
+++ src/app/chat/components/ChatForm/ChatForm.tsx
@@ -11,9 +11,37 @@ type Props = {
  */
 export const ChatForm: FC<Props> = ({ onSendButtonClick }) => {
   const [message, setMessage] = useState("");
+  const [images, setImages] = useState<
+    (string | ArrayBuffer | null | undefined)[]
+  >([]);
 
-  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const onChangeMessage = (e: ChangeEvent<HTMLInputElement>) => {
     setMessage(e.target.value);
+  };
+
+  const onChangeImages = (e: ChangeEvent<HTMLInputElement>) => {
+    // 何も選択されなかったら処理を中断
+    const length = e.target.files?.length || 0;
+    if (length === 0) return;
+
+    // 元々表示されていたプレビュー画像を削除
+    setImages([]);
+
+    // 選択された画像の枚数分処理を実行
+    [...Array(length)].map((_, index) => {
+      const reader = new FileReader();
+
+      // 画像読み込み後の処理を定義
+      reader.onload = (pe: ProgressEvent<FileReader>) => {
+        setImages((prev) => [...prev, pe.target?.result]);
+      };
+
+      const file = e.target.files?.[index];
+      if (!file) return;
+      if (!file.type.match("image.*")) return;
+
+      reader.readAsDataURL(file);
+    });
   };
 
   const onClick = () => {
@@ -25,26 +53,50 @@ export const ChatForm: FC<Props> = ({ onSendButtonClick }) => {
   };
 
   return (
-    <form className="flex w-full max-w-screen-sm gap-4">
-      <label>
-        <p className="py-2">📷</p>
-        <input name="image" type="file" accept="image/*" className="hidden" />
-      </label>
-      <input
-        name="message"
-        value={message}
-        onChange={onChange}
-        type="text"
-        placeholder="メッセージを入力…"
-        className="flex-1 rounded-sm p-2"
-      />
-      <button
-        type="button"
-        onClick={onClick}
-        className="rounded-sm bg-secondary p-2 px-4 text-white"
-      >
-        送信
-      </button>
-    </form>
+    <div className="flex flex-col justify-center gap-2">
+      <div className="flex w-full max-w-screen-sm gap-4 overflow-x-scroll">
+        {images.map((image, index) => {
+          return (
+            !!image && (
+              // eslint-disable-next-line @next/next/no-img-element
+              <img
+                src={image as string}
+                alt="画像のプレビュー"
+                key={index}
+                className="h-20"
+              />
+            )
+          );
+        })}
+      </div>
+      <form className="flex w-full max-w-screen-sm gap-4">
+        <label>
+          <p className="py-2">📷</p>
+          <input
+            name="image"
+            type="file"
+            accept="image/*"
+            onChange={onChangeImages}
+            multiple
+            className="hidden"
+          />
+        </label>
+        <input
+          name="message"
+          type="text"
+          value={message}
+          placeholder="メッセージを入力…"
+          onChange={onChangeMessage}
+          className="flex-1 rounded-sm p-2"
+        />
+        <button
+          type="button"
+          onClick={onClick}
+          className="bg-secondary rounded-sm p-2 px-4 text-white"
+        >
+          送信
+        </button>
+      </form>
+    </div>
   );
 };
