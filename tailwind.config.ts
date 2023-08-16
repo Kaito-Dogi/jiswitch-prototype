2493b82aa703b44fd7d7859815d4e76964ac885d
Kaito-Dogi <kendobu0405@gmail.com>
2023-08-17 03:23:23 +0900

    💄 カスタムの色を追加

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,11 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    colors: {
+      white: "#ffffff",
+      black: "#222222",
+      gray: "#e5e5e5",
+      primary: "#07c655",
     },
   },
   plugins: [],
