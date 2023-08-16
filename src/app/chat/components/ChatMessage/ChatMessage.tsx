d24af585b498d693ba3b27c02157c2c5af3c7837
Kaito-Dogi <kendobu0405@gmail.com>
2023-08-17 03:48:09 +0900

    🐛

--- src/app/chat/components/ChatMessage/ChatMessage.tsx
+++ src/app/chat/components/ChatMessage/ChatMessage.tsx
@@ -2,21 +2,21 @@ import { FC } from "react";
 
 type Props = {
   children: string;
-  fromUser?: boolean;
+  fromUser: boolean;
 };
 
 /**
  * @package
  */
-export const Message: FC<Props> = ({ children, fromUser = true }) => {
+export const ChatMessage: FC<Props> = ({ children, fromUser }) => {
   return fromUser ? (
-    <div className="grid place-items-end">
+    <div className="flex justify-end">
       <p className="max-w-[70%] rounded-l-lg rounded-tr-lg bg-primary p-2">
         {children}
       </p>
     </div>
   ) : (
-    <div className="grid place-content-start">
+    <div className="flex justify-start">
       <p className="max-w-[70%] rounded-r-lg rounded-tl-lg bg-white p-2">
         {children}
       </p>
