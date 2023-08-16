6764af8b214402d924f9337ec96d7a31be0f5bbd
Kaito-Dogi <kendobu0405@gmail.com>
2023-08-16 16:15:44 +0900

    🎨

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className='grid h-full place-items-center'>
-      <h1 className='text-center'>jiswitch prototype</h1>
+    <main className="grid h-full place-items-center">
+      <h1 className="text-center">jiswitch prototype</h1>
     </main>
-  )
+  );
 }
