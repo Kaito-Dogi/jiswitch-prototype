79477b955010d483cd96b7adddadf558604d2f34
Kaito-Dogi <kendobu0405@gmail.com>
2023-08-17 12:40:09 +0900

    ♻️ ChatImage を切り出し

---
+++ src/app/chat/components/ChatImage/ChatImage.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import { FC } from "react";
+
+import { Message } from "../../models/Message";
+
+type Props = {
+  src: Message["images"][number];
+};
+
+/**
+ * @package
+ */
+export const ChatImage: FC<Props> = ({ src }) => {
+  return (
+    <Image
+      src={src as string}
+      alt="画像"
+      width={0}
+      height={0}
+      className="h-20 w-20 object-cover"
+    />
+  );
+};
---
+++ src/app/chat/components/ChatImage/index.ts
@@ -0,0 +1,1 @@
+export { ChatImage } from "./ChatImage";
